7b679b59653b1d9385ac35acf713d848a7f0dea4
ryukk99 <75015350+ryukk99@users.noreply.github.com>
2022-08-03 15:02:17 +0530

    Add files via upload

---
+++ for loop in r programming.R
@@ -0,0 +1,13 @@
+## R ---- FOR loop in R programming ------
+for(y in 1:10){
+  print(y)
+}
+
+for(y in 1:10){
+  print(paste("number: ",y))
+}
+
+f<-c("orange","banana", "graps", "mango", "apple")
+for(i in f){
+  print(f)
+}
