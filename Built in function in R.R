3886bedabc0707fdcc765c534688e39d07c07391
ryukk99 <75015350+ryukk99@users.noreply.github.com>
2022-09-24 16:04:13 +0530

    Add files via upload

---
+++ Built in function in R.R
@@ -0,0 +1,52 @@
+x<- 5.6
+print(ceiling(x))
+print(floor(x))
+print(abs(x))
+print(sqrt(45))
+
+y<-c(1.5,5.6,8.6)
+print(trunc(y))
+
+z<-5
+print(sin(z))
+print(cos(z))
+print(log(z))
+print(exp(z))
+
+a<- "1234567899"
+substr(a,3,3)
+substr(a,5,7)
+
+s1<- "Monkey D LUFFy"
+print(tolower(s1))
+print(toupper(s1))
+
+
+#to search for perticular data grep fuction
+s2<- c('abcd','bcbd','abcdbcbd')
+var<-'^abc'
+print(grep(var,s2))
+
+
+A1<-c(0:10, 40)
+
+
+A2<-sum(A2)
+print(A2)
+
+max()
+min()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
